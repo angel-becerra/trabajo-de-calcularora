72a0a6f0eed9e67149e734d96c4ca9ece5f752eb
angel-becerra <57011926+angel-becerra@users.noreply.github.com>
2019-10-30 18:13:45 -0500

    Add files via upload

---
+++ trabajo04_becerra_angel.py
@@ -0,0 +1,230 @@
+#calculadora nro01
+#esta calculadora desarrolla el area de un triangulo
+
+#declaracion de variables
+base,altura,area_triangulo=0.0,0.0,0.0
+
+#calculadora
+base=18
+altura=24
+area_triangulo=(base*altura)/2
+
+#mostrar datos
+print("base es ", base)
+print("altura es ", altura)
+print("area_triangulo es", area_triangulo)
+
+#calculadora nro02
+#esta calculadora desarrolla el volumen de un paralepipedo
+
+#declaracion de variables
+base01,base02,altura,volumen_paralelepipedo=0.0,0.0,0.0,0.0
+
+#calculadora
+base01=12
+base02=16
+altura=8
+volumen_paralelepipedo=(base01*base02*altura)
+
+#mostrar datos
+print("base01 es", base01)
+print("base02 es", base02)
+print("altura es", altura)
+print("volumen_paralelepipedo es",volumen_paralelepipedo)
+
+#calculadora nro03
+#esta calculadora muestra la velocidad final de una particula en MRUV
+
+#declaracion de variables
+velocidad_inicial,velocidad_final,aceleracion,tiempo_movimiento=0.0,0.0,0.0,0.0
+
+#calculadora
+velocidad_inicial=12
+aceleracion=4
+tiempo_movimiento=5
+velocidad_final=( velocidad_inicial + (aceleracion*tiempo_movimiento))
+
+#mostrar datos
+print("velocidad_inicial es", velocidad_inicial)
+print("aceleracion es", aceleracion)
+print("tiempo_movimiento es", tiempo_movimiento)
+print("velocidad_final es", velocidad_final)
+
+#calculadora nro04
+#esta calculadora muestra la fuerza ejercida por una particula
+
+#declaracion de variables
+masa,aceleracion,fuerza_ejercida=0.0,0.0,0.0
+
+#calculadora
+masa=8
+aceleracion=24
+fuerza_ejercida=(masa*aceleracion)
+
+#mostrar datos
+print("masa es", masa)
+print("aceleracion es", aceleracion)
+print("fuerza_ejercida es", fuerza_ejercida)
+
+#calculadora nro05
+#esta calculadora muestra la energia cinetica de una particula
+
+#declaracion de variables
+masa,velocidad,energia_cinetica=0.0,0.0,0.0
+
+#calculadora
+masa=6
+velocidad=8
+energia_cinetica=(masa*velocidad*velocidad)/2
+
+#mostrar datos
+print("masa es", masa)
+print("velocidad es", velocidad)
+print("energia_cinetica es", energia_cinetica)
+
+#calculadora nro06
+#esta calculadora  muestra la presion hidrostatica
+
+#declaracion de variables
+densidad_liquido,gravedad,haltura,presion_hidrostatica=0.0,0.0,0.0,0.0
+
+#calculadora
+densidad_liquido=0.8
+gravedad=9.8
+haltura=6
+presion_hidrostatica=(densidad_liquido*gravedad*haltura)
+
+#mostrar datos
+print("densidad_liquido es", densidad_liquido)
+print("gravedad es", gravedad)
+print("haltura es", haltura)
+print("presion_hidrostatica es", presion_hidrostatica)
+
+#calculadora nro07
+#esta calculadora muestra la dilatacion lineal de uma particula al ser sometida a una variacion de temperatura
+
+#declaracion de variables
+coeficiente_diltacion,longitud_inicial,variacion_temperatura,dilatacion_lineal=0.0,0.0,0.0,0.0
+
+#calculadora
+coeficiente_diltacion=0.0008
+longitud_inicial=20
+variacion_temperatura=80
+dilatacion_lineal=(coeficiente_diltacion*longitud_inicial*variacion_temperatura)
+
+#mostar datos
+print("coeficiente_dilatacion es", coeficiente_diltacion)
+print("longitud_inicial es", longitud_inicial)
+print("variacion_temperatura es", variacion_temperatura)
+print("dilatacion_lineal es", dilatacion_lineal)
+
+#calculadora nro08
+#esta calculadora muestra la fuerza de repulsion entre dos cargas
+
+#declaracion de variables
+carga01,carga02,constante_electrica,distancia_entre_particulas,fuerza_de_repulsion=0.0,0.0,0.0,0.0,0.0
+
+#calculadora
+carga01=95
+carga02=45
+constante_electrica=9*10000000000000000
+distancia_entre_particulas=5
+fuerza_de_repulsion=( constante_electrica*carga02*carga01)/distancia_entre_particulas*distancia_entre_particulas
+
+#mostrar variables
+print("carga01 es", carga01)
+print("carga02 es", carga02)
+print("constante_electrica es", constante_electrica)
+print("distancia_entre_particulas es", distancia_entre_particulas)
+print("fuerza_de_repulsion es", fuerza_de_repulsion)
+
+#calculadora nro09
+#esta calculadora muestra el volumen del area de una esfera
+
+#declaracion de variables
+valor_aproximado_pi,radio_de_la_esfera=0.0,0.0
+
+#calculadora
+valor_aproximado_pi=3.14
+radio_de_la_esfera=4
+area_de_la_esfera=(4*valor_aproximado_pi*radio_de_la_esfera*radio_de_la_esfera)
+
+#mostrar datos
+print("valor_aproximado_pi es", valor_aproximado_pi)
+print("radio_de_la_esfera es", radio_de_la_esfera)
+print("area_de_la_esfera es", area_de_la_esfera)
+
+#calculadora nro10
+#esta calculadora muestra el volumen de la esfera
+
+ #declaracion de variables
+valor_aproximado_pi,radio_de_la_esfera,volumen_de_la_esfera=0.0,0.0,0.0
+
+#calculadora
+valor_aproximado_pi=3.14
+radio_de_la_esfera=9
+volumen_de_la_esfera=( 4*valor_aproximado_pi*radio_de_la_esfera*radio_de_la_esfera*radio_de_la_esfera)/3
+
+#mostrar datos
+print("valor_aproximado_pi es", valor_aproximado_pi)
+print("radio_de_la_esfera es", radio_de_la_esfera)
+print("volumen_de_la_esfera es", volumen_de_la_esfera)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
